0add4ed0589da3f2842c8d77a3197ebc6fd3224e
perkles <maikosilvae@gmail.com>
2022-03-30 12:57:18 -0300

    service

--- src/main/java/com/codex/apostas/service/ApostadorService.java
+++ src/main/java/com/codex/apostas/service/ApostadorService.java
@@ -1,8 +1,10 @@
 package com.codex.apostas.service;
 
+import com.codex.apostas.model.Apostador;
 import com.codex.apostas.model.dto.ApostadorIn;
 import com.codex.apostas.repository.ApostadorRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -10,9 +12,11 @@ import org.springframework.stereotype.Service;
 public class ApostadorService {
 
     private final ApostadorRepository apostadorRepository;
+    private final ModelMapper modelMapper;
 
     public void salvarApostador(ApostadorIn apostadorIn){
-        apostadorRepository.save();
+        Apostador apostador = modelMapper.map(apostadorIn, Apostador.class);
+        apostadorRepository.save(apostador);
 
     }
 }
