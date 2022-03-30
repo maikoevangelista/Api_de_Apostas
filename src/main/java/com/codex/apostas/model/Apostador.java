b5b4ae46a1afb428646477dca3a7187e1a478040
perkles <maikosilvae@gmail.com>
2022-03-30 13:07:01 -0300

    setter

--- src/main/java/com/codex/apostas/model/Apostador.java
+++ src/main/java/com/codex/apostas/model/Apostador.java
@@ -3,14 +3,15 @@ package com.codex.apostas.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@AllArgsConstructor
 @Getter
+@Setter
 @Entity
 @NoArgsConstructor
 public class Apostador {
