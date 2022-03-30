fe0824790aab696dc7a1cc3fa90d04935dc05398
perkles <maikosilvae@gmail.com>
2022-03-30 09:52:29 -0300

    controller

--- src/main/java/com/codex/apostas/ApostadorController.java
+++ src/main/java/com/codex/apostas/ApostadorController.java
@@ -1,6 +1,5 @@
 package com.codex.apostas;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,9 +10,16 @@ import static org.springframework.http.HttpStatus.CREATED;
 @RestController
 public class ApostadorController {
 
+    private final ApostadorRepository apostadorRepository;
+
+    public ApostadorController(ApostadorRepository apostadorRepository) {
+        this.apostadorRepository = apostadorRepository;
+    }
+
     @PostMapping("/apostador")
     public ResponseEntity salvarApostador(@RequestBody ApostadorIn apostadorIn) {
         Apostador apostador = apostadorIn.toConvert();
+        apostadorRepository.save(apostador);
 
         return ResponseEntity.status(CREATED).build();
     }
